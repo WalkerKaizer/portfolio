e23bb083a93300d5d6a486e25f0a525395f00da2
Franck <allanfranckgnele@gmail.com>
2025-01-17 10:15:19 +0100

    update

---
+++ conception web.js
@@ -0,0 +1,56 @@
+
+window.addEventListener('load', function () {
+
+    setTimeout(lazyLoad, 1000);
+
+});
+
+function lazyLoad() {
+    var card_images = document.querySelectorAll('.card-image');
+
+    card_images.forEach(function (card_image) {
+        var image_url = card_image.getAttribute('data-image-full');
+        var content_image = card_image.querySelector('img');
+
+        content_image.src = image_url;
+
+        content_image.addEventListener('load', function () {
+            card_image.style.backgroundImage = 'url(' + image_url + ')';
+            card_image.className = card_image.className + ' is-loaded';
+        });
+
+    });
+    var sidenav = document.getElementById("mySidenav");
+    var openBtn = document.getElementById("openBtn");
+    var closeBtn = document.getElementById("closeBtn");
+
+    openBtn.onclick = openNav;
+    closeBtn.onclick = closeNav;
+
+    function openNav() {
+        sidenav.classList.add("active");
+    }
+
+    function closeNav() {
+        sidenav.classList.remove("active");
+    }
+
+    // Gestion du sous-menu
+    var montageMenu = document.getElementById("montageMenu");
+    var sousMenu = montageMenu.querySelector(".sous-menu");
+
+    // Afficher le sous-menu au survol
+    montageMenu.addEventListener('mouseenter', function () {
+        sousMenu.style.display = 'block';
+    });
+
+    // Cacher le sous-menu lorsque la souris quitte
+    montageMenu.addEventListener('mouseleave', function () {
+        sousMenu.style.display = 'none';
+    })
+
+}
+
+
+
+
--- loader.js
+++ loader.js
@@ -24,12 +24,5 @@ montageMenu.addEventListener('mouseenter', function () {
 montageMenu.addEventListener('mouseleave', function () {
     sousMenu.style.display = 'none';
 });
-window.addEventListener("load", function () {
-    // Garde le loader visible pendant 3 secondes (3000ms)
-    setTimeout(() => {
-        // Masquer le loader
-        document.getElementById("loader").style.display = "none";
 
-        document.getElementById("content").style.display = "block";
-    }, 3000); // 3000ms = 3 secondes
-});
+
